64debbb8acba51616eef0213fbf1c5655ab8e184
dentych <dennis@tychsen.me>
2017-09-14 23:51:06 +0200

    Fix build problem

--- src/app/log-match/log-match.module.ts
+++ src/app/log-match/log-match.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MyMaterialModule} from '../my-material/my-material.module';
 import {AppLogMatchComponent} from './app-log-match/app-log-match.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MyMaterialModule
   ],
   declarations: [
